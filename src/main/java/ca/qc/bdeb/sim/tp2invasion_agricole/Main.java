85c2c593acbf75db0efce36e864c69b69c65c77d
GeorgesAbiChahine <georgesabichahine1@gmail.com>
2024-10-28 09:15:08 -0400

    ArrierePlan

--- src/main/java/ca/qc/bdeb/sim/tp2invasion_agricole/Main.java
+++ src/main/java/ca/qc/bdeb/sim/tp2invasion_agricole/Main.java
@@ -2,19 +2,37 @@ package ca.qc.bdeb.sim.tp2invasion_agricole;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+    public static int LARGEUR = 900;
+    public static int HAUTEUR = 520;
     @Override
     public void start(Stage stage) throws IOException {
       var root = new Pane();
-        Scene scene = new Scene(root, 320, 240);
+        Scene scene = creerSceneJeu(root);
+
         stage.setTitle("Invasion Agricole");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private Scene creerSceneJeu(Pane root){
+        var scene = new Scene(root, LARGEUR, HAUTEUR);
+        var canvas = new Canvas(LARGEUR,HAUTEUR);
+
+
+        GraphicsContext contexte = canvas.getGraphicsContext2D();
+
+
+
+        return scene;
     }
 
     public static void main(String[] args) {
