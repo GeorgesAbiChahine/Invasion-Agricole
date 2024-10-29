be9ac757f1f2fa0b27940ef54aa7dd6abbf9b166
GeorgesAbiChahine <georgesabichahine1@gmail.com>
2024-10-28 23:33:49 -0400

    28 oct 11:33 changement dans Aimant

--- src/main/java/ca/qc/bdeb/sim/tp2invasion_agricole/pkgpartie/pkgentites/pkgprojectiles/Aimant.java
+++ src/main/java/ca/qc/bdeb/sim/tp2invasion_agricole/pkgpartie/pkgentites/pkgprojectiles/Aimant.java
@@ -4,7 +4,7 @@ import ca.qc.bdeb.sim.tp2invasion_agricole.pkgpartie.pkgentites.Entite;
 import javafx.scene.image.Image;
 
 public class Aimant extends Entite {
-    public Aimant(double ax, double ay, double vx, double vy, double LARGEUR, double HAUTEUR, Image IMAGE, double x, double y) {
-        super(ax, ay, vx, vy, LARGEUR, HAUTEUR, IMAGE, x, y);
+    public Aimant(double ax, double ay, double vx, double vy, Image IMAGE, double x, double y) {
+            super(ax, 0, vx, vy, 42, 40, new Image("aimant.png"), x, y);
     }
 }
